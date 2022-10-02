da9d160fde614885d2497df5ce8bacffc1f34a9b
Satyam <satyam10180179@gmail.com>
2022-10-02 14:31:19 +0530

    fixed db models.py

--- scripts/models.py
+++ scripts/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime, UniqueConstraint
 
 Base = declarative_base()
 
@@ -7,8 +7,11 @@ Base = declarative_base()
 class Stocks(Base):
     """"""
     id = Column(Integer, primary_key=True)
-    series = Column(String, not_null=True)
-    closing_price = Column(Float, not_null=True)
-    date = Column(DateTime, not_null=True)
+    series = Column(String, nullable=False)
+    closing_price = Column(Float, nullable=False)
+    date = Column(DateTime, nullable=False)
 
     __tablename__ = "stocks"
+    __table_args__ = (
+        UniqueConstraint("series", "date", name="series_date_unique_constraint"),
+    )
