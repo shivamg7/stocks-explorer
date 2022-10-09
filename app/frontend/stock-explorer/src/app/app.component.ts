52191f4cb91ce1bdfacd6fd553e6de0cde35f72c
shivam <shivamxav@gmail.com>
2022-10-09 13:51:19 +0530

    fix no data alert

--- app/frontend/stock-explorer/src/app/app.component.ts
+++ app/frontend/stock-explorer/src/app/app.component.ts
@@ -40,6 +40,7 @@ export class AppComponent {
     });
     this.yesterdayDate = this._getYesterdayDate();
     this.formGroup.valueChanges.subscribe(() => {
+      this.noStockData = false;
       this.closingPrice = undefined;
     })
   }
