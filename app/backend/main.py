91b2442c4375ef43f756417a47e3f1987484f920
shivam <shivamxav@gmail.com>
2022-10-09 13:44:30 +0530

    enable cors for backend

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,12 +1,21 @@
 from typing import Union
 
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from app.backend.core import crud
 from app.backend.core.database import SessionLocal
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 def get_db():
@@ -29,9 +38,9 @@ def all_stocks(db: Session = Depends(get_db)):
     return stocks
 
 
-@app.get("/stocks/{series}/{date}")
+@app.get("/stocks/series")
 def get_stock_by_series_date(series: str, date: str, db: Session = Depends(get_db)):
     stock = crud.get_stock_by_series_date(db=db, series=series, date=date)
     if not stock:
-        raise HTTPException(status_code=404, detail="stock data not found")
+        raise HTTPException(status_code=404, detail="data not found")
     return stock
